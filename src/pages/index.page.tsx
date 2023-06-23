3c70be33aa162d580f6d1b7942817832beca0118
Renan <renan.fachin@hotmail.com>
2023-06-23 19:10:47 -0300

    Adiciona configuração de pageExtensions

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,4 +1,4 @@
-import { Heading, Text } from '@ignite-ui/react'
+import { Heading } from '@ignite-ui/react'
 
 import { Roboto_Flex as Roboto } from 'next/font/google'
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
