ad0e3d1f5974416e7369d458ed709acf9392bc0b
Renan <renan.fachin@hotmail.com>
2023-06-27 18:22:06 -0300

    Salvando dados de pré registro nos cookies

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,18 +1,19 @@
 import { prisma } from '@/src/lib/prisma'
+import { setCookie } from 'nookies'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
-  request: NextApiRequest,
-  response: NextApiResponse,
+  req: NextApiRequest,
+  res: NextApiResponse,
 ) {
   // Fazendo uma verificação do método que está sendo chamado
   // Fazendo com que todos os métodos diferente do POST sejam redirecionados para um erro
-  if (request.method !== 'POST') {
-    return response.status(405).end()
+  if (req.method !== 'POST') {
+    return res.status(405).end()
   }
 
   // Agora sabendo que a rota é do tipo POST, persistir o usuário no db
-  const { name, username } = request.body
+  const { name, username } = req.body
 
   // Validando se o usuário já existe no db
   const userExists = await prisma.user.findUnique({
@@ -22,7 +23,7 @@ export default async function handler(
   })
 
   if (userExists) {
-    return response.status(400).json({
+    return res.status(400).json({
       message: 'username already taken.',
     })
   }
@@ -34,5 +35,11 @@ export default async function handler(
     },
   })
 
-  return response.status(201).json(user)
+  // Após a criação do usuário, salvar os dados no cookie
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/', // todas as rotas da aplicação podem acessar
+  })
+
+  return res.status(201).json(user)
 }
