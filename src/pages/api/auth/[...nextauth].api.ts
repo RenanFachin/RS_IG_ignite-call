1263a627ae8049c616597a411213ddd0c982c9e5
Renan <renan.fachin@hotmail.com>
2023-06-28 17:58:49 -0300

    Definindo o escopo de autorização da autenticação social

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,11 +1,20 @@
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+const userInfoEmail = 'https://www.googleapis.com/auth/userinfo.email'
+const userInfoProfile = 'https://www.googleapis.com/auth/userinfo.profile'
+const userInfoCalendar = 'https://www.googleapis.com/auth/calendar'
+
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          scope: `${userInfoEmail} ${userInfoProfile} ${userInfoCalendar}`,
+        },
+      },
     }),
   ],
 }
