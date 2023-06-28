fe62736a6067eb99792d304f91482190249019a7
Renan <renan.fachin@hotmail.com>
2023-06-28 17:23:57 -0300

    Cria rota de autenticação com NextAuth e provider de autenticação para o app

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import { globalStyles } from '../styles/global'
 
 import { Roboto_Flex as Roboto } from 'next/font/google'
@@ -7,10 +8,15 @@ const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 // Executando a função de estilos globais
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <div className={`${roboto.variable} font-sans`}>
-      <Component {...pageProps} />
-    </div>
+    <SessionProvider session={session}>
+      <div className={`${roboto.variable} font-sans`}>
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
   )
 }
