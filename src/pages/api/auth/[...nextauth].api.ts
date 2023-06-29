c964260a02485ee1063a673735d511c6e679c192
Renan <renan.fachin@hotmail.com>
2023-06-29 18:42:58 -0300

    Finaliza fluxo de autenticação e criação de session com nextauth e google + nosso db

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,39 +1,60 @@
 import { PrismaAdapter } from '@/src/lib/auth/prisma-adapter'
 import NextAuth, { NextAuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
+
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 const userInfoEmail = 'https://www.googleapis.com/auth/userinfo.email'
 const userInfoProfile = 'https://www.googleapis.com/auth/userinfo.profile'
 const userInfoCalendar = 'https://www.googleapis.com/auth/calendar'
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(),
+export function buildNextAuthOptions(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
 
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          scope: `${userInfoEmail} ${userInfoProfile} ${userInfoCalendar}`,
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        authorization: {
+          params: {
+            scope: `${userInfoEmail} ${userInfoProfile} ${userInfoCalendar}`,
+          },
         },
+        // método profile = mapeia os campos internos do usuário com o perfil retornado do google
+        profile(profile: GoogleProfile) {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            username: '',
+            email: profile.email,
+            avatar_url: profile.picture,
+          }
+        },
+      }),
+    ],
+
+    callbacks: {
+      async signIn({ account }) {
+        // Dentro do account teremos acesso as permissões que o usuário deu para a aplicação
+        if (!account?.scope?.includes(`${userInfoCalendar}`)) {
+          // Caso ele não tenha dado a permissão - Retornar para a página de registro
+          return '/register/connect-calendar/?error=permissions'
+
+          // Enviando um query params para sinalizar que deu um erro
+        }
+
+        // Precisamos retornar um true para deixar a autenticação seguir o fluxo
+        return true
       },
-    }),
-  ],
-
-  callbacks: {
-    async signIn({ account }) {
-      // Dentro do account teremos acesso as permissões que o usuário deu para a aplicação
-      if (!account?.scope?.includes(`${userInfoCalendar}`)) {
-        // Caso ele não tenha dado a permissão - Retornar para a página de registro
-        return '/register/connect-calendar/?error=permissions'
-
-        // Enviando um query params para sinalizar que deu um erro
-      }
-
-      // Precisamos retornar um true para deixar a autenticação seguir o fluxo
-      return true
     },
-  },
+  }
 }
-export default NextAuth(authOptions)
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  // Do whatever you want here, before the request is passed down to `NextAuth`
+  return await NextAuth(req, res, buildNextAuthOptions(req, res))
+}
