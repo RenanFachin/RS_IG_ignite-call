a46fe76ea37e944aeeb70c6d9a818914f071541d
Renan <renan.fachin@hotmail.com>
2023-07-04 17:39:07 -0300

    Configura react-query

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -5,6 +5,8 @@ import { SessionProvider } from 'next-auth/react'
 import { globalStyles } from '../styles/global'
 
 import { Roboto_Flex as Roboto } from 'next/font/google'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '../lib/react-query'
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 
 // Executando a função de estilos globais
@@ -15,10 +17,12 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <div className={`${roboto.variable} font-sans`}>
-        <Component {...pageProps} />
-      </div>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <div className={`${roboto.variable} font-sans`}>
+          <Component {...pageProps} />
+        </div>
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
